4d0446c2fe0dd088f53aada452ebb07f5830cd0a
Asma Smail <asma.smail.halouane@gmail.com>
2021-12-28 14:53:49 +0100

    Initial commit

---
+++ project-6/truffle.js
@@ -0,0 +1,21 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const infuraKey = "afe017499176473482c7d21febbd56dc";
+const mnemonic = "poverty lottery find secret flee gown nothing priority toy acoustic abuse west";
+
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*" // Match any network id
+    },
+    ropsten: {
+      provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/${infuraKey}`),
+      network_id: 3,       // Ropsten's id
+      gas: 5500000,        // Ropsten has a lower block limit than mainnet
+      // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    }
+  }
+};
